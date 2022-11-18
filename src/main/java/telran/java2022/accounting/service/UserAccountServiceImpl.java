7a04cccc25cb7797aa9d76b1d8b2b4108b74e925
GenaTkach <takachgena@gmail.com>
2022-11-19 00:51:16 +0200

    Added :
    1)SessionId
    2)Context for efficienty (берем юзеров из репозитория и помещаем в
    локальный контекст)

--- src/main/java/telran/java2022/accounting/service/UserAccountServiceImpl.java
+++ src/main/java/telran/java2022/accounting/service/UserAccountServiceImpl.java
@@ -2,6 +2,7 @@ package telran.java2022.accounting.service;
 
 import org.mindrot.jbcrypt.BCrypt;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -16,70 +17,86 @@ import telran.java2022.accounting.model.UserAccount;
 
 @Service
 @RequiredArgsConstructor
-public class UserAccountServiceImpl implements UserAccountService {
-	final UserAccountRepository repository;
-	final ModelMapper modelMapper;
+public class UserAccountServiceImpl implements UserAccountService, CommandLineRunner {
+    final UserAccountRepository repository;
+    final ModelMapper modelMapper;
 
+    @Override
+    public UserAccountResponseDto addUser(UserRegisterDto userRegisterDto) {
+	if (repository.existsById(userRegisterDto.getLogin())) {
+	    throw new UserExistsException(userRegisterDto.getLogin());
+	}
+	UserAccount userAccount = modelMapper.map(userRegisterDto, UserAccount.class);
+	String password = BCrypt.hashpw(userRegisterDto.getPassword(), BCrypt.gensalt());
+	userAccount.setPassword(password);
+	repository.save(userAccount);
+	return modelMapper.map(userAccount, UserAccountResponseDto.class);
+    }
 
-	@Override
-	public UserAccountResponseDto addUser(UserRegisterDto userRegisterDto) {
-		if (repository.existsById(userRegisterDto.getLogin())) {
-			throw new UserExistsException(userRegisterDto.getLogin());
-		}
-		UserAccount userAccount = modelMapper.map(userRegisterDto, UserAccount.class);
-		String password = BCrypt.hashpw(userRegisterDto.getPassword(), BCrypt.gensalt());
-		userAccount.setPassword(password);
-		repository.save(userAccount);
-		return modelMapper.map(userAccount, UserAccountResponseDto.class);
+    @Override
+    public UserAccountResponseDto getUser(String login) {
+	UserAccount userAccount = repository.findById(login)
+		.orElseThrow(() -> new UserNotFoundException());
+	return modelMapper.map(userAccount, UserAccountResponseDto.class);
+    }
+
+    @Override
+    public UserAccountResponseDto removeUser(String login) {
+	UserAccount userAccount = repository.findById(login)
+		.orElseThrow(() -> new UserNotFoundException());
+	repository.deleteById(login);
+	return modelMapper.map(userAccount, UserAccountResponseDto.class);
+    }
+
+    @Override
+    public UserAccountResponseDto editUser(String login, UserUpdateDto updateDto) {
+	UserAccount userAccount = repository.findById(login)
+		.orElseThrow(() -> new UserNotFoundException());
+	if (updateDto.getFirstName() != null) {
+	    userAccount.setFirstName(updateDto.getFirstName());
+	}
+	if (updateDto.getLastName() != null) {
+	    userAccount.setLastName(updateDto.getLastName());
+	}
+	repository.save(userAccount);
+	return modelMapper.map(userAccount, UserAccountResponseDto.class);
+    }
+
+    @Override
+    public RolesResponseDto changeRolesList(String login, String role, boolean isAddRole) {
+	UserAccount userAccount = repository.findById(login)
+		.orElseThrow(() -> new UserNotFoundException());
+	boolean res;
+	if (isAddRole) {
+	    res = userAccount.addRole(role.toUpperCase());
+	} else {
+	    res = userAccount.removeRole(role.toUpperCase());
+	}
+	if (res) {
+	    repository.save(userAccount);
+	}
+	return modelMapper.map(userAccount, RolesResponseDto.class);
+    }
+
+    @Override
+    public void changePassword(String login, String newPassword) {
+	UserAccount userAccount = repository.findById(login)
+		.orElseThrow(() -> new UserNotFoundException());
+	String password = BCrypt.hashpw(newPassword, BCrypt.gensalt());
+	userAccount.setPassword(password);
+	repository.save(userAccount);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+	if (!repository.existsById("admin")) {
+	    String password = BCrypt.hashpw("admin", BCrypt.gensalt());
+	    UserAccount userAccount = new UserAccount("admin", password, "", "");
+	    userAccount.addRole("MODERATOR");
+	    userAccount.addRole("ADMINISTRATOR");
+	    repository.save(userAccount);
 	}
 
-	@Override
-	public UserAccountResponseDto getUser(String login) {
-		UserAccount userAccount = repository.findById(login).orElseThrow(() -> new UserNotFoundException());
-		return modelMapper.map(userAccount, UserAccountResponseDto.class);
-	}
-
-	@Override
-	public UserAccountResponseDto removeUser(String login) {
-		UserAccount userAccount = repository.findById(login).orElseThrow(() -> new UserNotFoundException());
-		repository.deleteById(login);
-		return modelMapper.map(userAccount, UserAccountResponseDto.class);
-	}
-
-	@Override
-	public UserAccountResponseDto editUser(String login, UserUpdateDto updateDto) {
-		UserAccount userAccount = repository.findById(login).orElseThrow(() -> new UserNotFoundException());
-		if (updateDto.getFirstName() != null) {
-			userAccount.setFirstName(updateDto.getFirstName());
-		}
-		if (updateDto.getLastName() != null) {
-			userAccount.setLastName(updateDto.getLastName());
-		}
-		repository.save(userAccount);
-		return modelMapper.map(userAccount, UserAccountResponseDto.class);
-	}
-
-	@Override
-	public RolesResponseDto changeRolesList(String login, String role, boolean isAddRole) {
-		UserAccount userAccount = repository.findById(login).orElseThrow(() -> new UserNotFoundException());
-		boolean res;
-		if (isAddRole) {
-			res = userAccount.addRole(role.toUpperCase());
-		} else {
-			res = userAccount.removeRole(role.toUpperCase());
-		}
-		if (res) {
-			repository.save(userAccount);
-		}	
-		return modelMapper.map(userAccount, RolesResponseDto.class);
-	}
-
-	@Override
-	public void changePassword(String login, String newPassword) {
-		UserAccount userAccount = repository.findById(login).orElseThrow(() -> new UserNotFoundException());
-		String password = BCrypt.hashpw(newPassword, BCrypt.gensalt());
-		userAccount.setPassword(password);
-		repository.save(userAccount);
-	}
+    }
 
 }
--- src/main/java/telran/java2022/security/filter/AuthenticationFilter.java
+++ src/main/java/telran/java2022/security/filter/AuthenticationFilter.java
@@ -22,74 +22,89 @@ import telran.java2022.accounting.dao.UserAccountRepository;
 import telran.java2022.accounting.model.UserAccount;
 import telran.java2022.security.context.SecurityContext;
 import telran.java2022.security.context.User;
+import telran.java2022.security.service.SessionService;
 
 @Component
 @RequiredArgsConstructor
 @Order(10)
 public class AuthenticationFilter implements Filter {
-	
-	final UserAccountRepository userAccountRepository;
-	final SecurityContext context;
+
+    final UserAccountRepository userAccountRepository;
+    final SecurityContext context;
+    final SessionService sessionService;
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+	    throws IOException, ServletException {
+	HttpServletRequest request = (HttpServletRequest) req;
+	HttpServletResponse response = (HttpServletResponse) resp;
+
+	if (checkEndPoint(request.getMethod(), request.getServletPath())) {
+
+	    // Getting Session ID
+	    String sessionId = request.getSession()
+		    .getId();
+	    UserAccount userCookies = sessionService.getUser(sessionId);
+	    
+	    // Если такой сессии не существует
+	    if(userCookies == null) {
+		String token = request.getHeader("Authorization");
+		    if (token == null) {
+			response.sendError(401);
+			return;
+		    }
+		    String[] credentials;
+		    try {
+			credentials = getCredentialsFromToken(token);
+		    } catch (Exception e) {
+			response.sendError(401, "Invalid token");
+			return;
+		    }
+		    UserAccount userAccount = userAccountRepository.findById(credentials[0])
+			    .orElse(null);
+		    if (userAccount == null || !BCrypt.checkpw(credentials[1], userAccount.getPassword())) {
+			response.sendError(401, "login or password is invalid");
+			return;
+		    }
+		    sessionService.addUser(sessionId, userAccount);
+	    }
+	    request = new WrappedRequest(request, userCookies.getLogin());
+	    User user = User.builder()
+		    .userName(userCookies.getLogin())
+		    .password(userCookies.getPassword())
+		    .roles(userCookies.getRoles())
+		    .build();
+	    context.addUser(user);
+	}
+	chain.doFilter(request, response);
+    }
+
+    private String[] getCredentialsFromToken(String token) {
+	String[] basicAuth = token.split(" ");
+	String decode = new String(Base64.getDecoder()
+		.decode(basicAuth[1]));
+	String[] credentials = decode.split(":");
+	return credentials;
+    }
+
+    private boolean checkEndPoint(String method, String servletPath) {
+	return !("POST".equalsIgnoreCase(method) && servletPath.matches("/account/register/?")
+		|| (servletPath.matches("/forum/posts(/\\w+)+/?")));
+    }
+
+    private class WrappedRequest extends HttpServletRequestWrapper {
+	String login;
+
+	public WrappedRequest(HttpServletRequest request, String login) {
+	    super(request);
+	    this.login = login;
+	}
 
 	@Override
-	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
-			throws IOException, ServletException {
-		HttpServletRequest request = (HttpServletRequest) req;
-		HttpServletResponse response = (HttpServletResponse) resp;
-		if (checkEndPoint(request.getMethod(), request.getServletPath())) {
-			String token = request.getHeader("Authorization");
-			if(token == null) {
-				response.sendError(401);
-				return;
-			}
-			String[] credentials;
-			try {
-				credentials = getCredentialsFromToken(token);
-			} catch (Exception e) {
-				response.sendError(401, "Invalid token");
-				return;
-			}
-			UserAccount userAccount = userAccountRepository.findById(credentials[0]).orElse(null);
-			if(userAccount == null || !BCrypt.checkpw(credentials[1], userAccount.getPassword())) {
-				response.sendError(401, "login or password is invalid");
-				return;
-			}
-			request = new WrappedRequest(request, userAccount.getLogin());
-			User user = User.builder()
-								.userName(userAccount.getLogin())
-								.password(userAccount.getPassword())
-								.roles(userAccount.getRoles())
-								.build();
-			context.addUser(user);
-		}
-		chain.doFilter(request, response);
+	public Principal getUserPrincipal() {
+	    return () -> login;
 	}
 
-	private String[] getCredentialsFromToken(String token) {
-		String[] basicAuth = token.split(" ");
-		String decode = new String(Base64.getDecoder().decode(basicAuth[1]));
-		String[] credentials = decode.split(":");
-		return credentials;
-	}
-
-	private boolean checkEndPoint(String method, String servletPath) {
-		return !("POST".equalsIgnoreCase(method) && servletPath.matches("/account/register/?")
-				|| (servletPath.matches("/forum/posts(/\\w+)+/?")));
-	}
-	
-	private class WrappedRequest extends HttpServletRequestWrapper {
-		String login;
-		
-		public WrappedRequest(HttpServletRequest request, String login) {
-			super(request);
-			this.login = login;
-		}
-		
-		@Override
-		public Principal getUserPrincipal() {
-			return () -> login;
-		}
-		
-	}
+    }
 
 }
